ebf4059a398cf4b96d0c1406a92be96bf079bcd3
Alex Benchea <alexbenchea99@yahoo.com>
2019-10-13 13:25:01 +0300

    Exception - must review

--- app.py
+++ app.py
@@ -9,21 +9,26 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route('/schedule', methods=['GET'])
 @cross_origin()
 def getTable():
+        try:
+                group = request.args.get('group')
+                semigroup = request.args.get('semigroup')
+                semester = request.args.get('semester')
+                year = request.args.get('year')
+                day = request.args.get('day')
+                
+                conf = Configuration()
+                conf.completeWebUrl(year,semester, group)
+                
+                link = conf.getWebUrl()
+                linkRoom = conf.getWebUrlRoom()
 
-        group = request.args.get('group')
-        semigroup = request.args.get('semigroup')
-        semester = request.args.get('semester')
-        year = request.args.get('year')
-        day = request.args.get('day')
-        
-        conf = Configuration()
-        conf.completeWebUrl(year,semester, group)
-        
-        link = conf.getWebUrl()
-        linkRoom = conf.getWebUrlRoom()
-
-        scCon = ScheduleController(group,semester,year,link,linkRoom,semigroup)
-        return scCon.getSmartSchedule()
+                scCon = ScheduleController(group,semester,year,link,linkRoom,semigroup)
+                return scCon.getSmartSchedule()
+        except:
+                data = dict()
+                data["Error"] = "Datele introduse sunt invalide"
+                return data
+                        
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0')
